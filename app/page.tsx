c678ab7c10947092ae4d2847fac8a26bcea3fcfe
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-17 09:08:29 -0500

    revised

--- app/page.tsx
+++ app/page.tsx
@@ -94,22 +94,22 @@
 //   );
 // }
 
-// export default function Page() {
-//   return <h1>Hello, Next.js!</h1>;
-// }
-
-"use client";
-
-import { useEffect } from "react";
-
 export default function Page() {
-  useEffect(() => {
-    console.time("MYTIMER");
-
-    setTimeout(() => {
-      console.timeEnd("MYTIMER");
-    }, 500); // Adding a 500 ms delay
-  }, []);
-
   return <h1>Hello, Next.js!</h1>;
 }
+
+// "use client";
+
+// import { useEffect } from "react";
+
+// export default function Page() {
+//   useEffect(() => {
+//     console.time("MYTIMER");
+
+//     setTimeout(() => {
+//       console.timeEnd("MYTIMER");
+//     }, 500); // Adding a 500 ms delay
+//   }, []);
+
+//   return <h1>Hello, Next.js!</h1>;
+// }
